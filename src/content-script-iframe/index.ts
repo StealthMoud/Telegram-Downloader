a38846ef8c465fec62f7a88c99e83d2915ca3f2d
StealthMoud <StealthMoud@gmail.com>
2026-03-02 02:07:13 +0100

    feat: content script foundation and constants

---
+++ src/content-script-iframe/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Definitive "Audio-Style" Restoration
+ * Removes FAB and non-working buttons.
+ * Implements "Play & Capture" for Video and Audio.
+ */
+
+const PROCESSED_ATTR = 'data-tg-dl-processed';
+const DOWNLOAD_EVENT = 'media_download_event';
+
+// --- HELPERS ---
